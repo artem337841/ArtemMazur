7a3669033ed6a6c9e10a4337f8e9b02100bf38ba
artem <mazurartemdmitrovih@gmail.com>
2025-03-11 18:54:27 +0200

    Add files  + player movement

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float horizontalInput;
+    public float speed;
+    public float xRange = 15;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        horizontalInput = Input.GetAxis("Horizontal");
+        transform.Translate(Vector3.right * speed * horizontalInput * Time.deltaTime);
+
+        if (transform.position.x < -xRange)
+        {
+            transform.position = new Vector3(-xRange, transform.position.y);
+        }
+
+        if (transform.position.x > xRange)
+        {
+            transform.position = new Vector3(xRange, transform.position.y);
+        }
+    }
+}
